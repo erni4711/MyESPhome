74e1602f47c1da46e9417a9e5aab4da2c3c01a53
erni4711 <Bernd.Oerding@arcor.de>
2026-09-07 21:29:31 +0200

    feat: Enhance logging for Home Assistant integration and clean up unused code

--- config/external_components/hatilvgl/tile_widget_clock.cpp
+++ config/external_components/hatilvgl/tile_widget_clock.cpp
@@ -4,8 +4,7 @@
 #include <ctime>
 #include <cstdio>
 
-// Diagnostic build: disable every LVGL event callback registration.
-#define lv_obj_add_event_cb(...) /* disabled for LVGL crash isolation */
+
 
 namespace web_admin_local {
 
