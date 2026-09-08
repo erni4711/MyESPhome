9171efed8264f1b0199d2fb4a957b83fba242df9
erni4711 <Bernd.Oerding@arcor.de>
2026-09-08 21:02:58 +0200

    Add climate and media widget support, along with settings tile
    - Introduced climate widget functionality with temperature adjustment capabilities.
    - Added media widget to display media player state and controls.
    - Implemented a settings tile for adjusting brightness and screen timeout.
    - Created camera tile for displaying camera state.
    - Enhanced weather widget to handle daily and hourly forecasts.
    - Updated the API to support setting climate temperature and controlling media playback.

--- config/external_components/hatifonts/mdi_icons_48.c
+++ config/external_components/hatifonts/mdi_icons_48.c
@@ -9,11 +9,7 @@
 //#include "src/devices/device_select.h"
 
 #ifndef MDI_ICONS_48
-#if defined(DEVICE_LAYOUT_1024X600) || defined(DEVICE_LAYOUT_480X480)
-#define MDI_ICONS_48 0
-#else
 #define MDI_ICONS_48 1
-#endif
 #endif
 
 #if MDI_ICONS_48
